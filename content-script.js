9e6d59d52aaf3376a0a19531a91d24b338692862
Will Bamberg <wbamberg@mozilla.com>
2018-10-25 17:13:42 -0700

    Updated for CodePen integration

--- content-script.js
+++ content-script.js
@@ -1,82 +1,73 @@
-const main = 'https://developer.mozilla.org/en-US/docs/User:wbamberg/querySelectorExample';
-const iframe = 'https://interactive-examples.mdn.mozilla.net/pages/webapi-tabbed/document-queryselector.html';
+let openButton = document.querySelector('.codepen-button');
+openButton.addEventListener('click', openCodePen);
+openButton.style.boxSizing = 'border-box';
 
-let href = document.location.href;
+function openCodePen() {
+    let codepenable = document.querySelector('.codepenable');
 
-if (href === iframe) {
-    console.log(href);
+    let outputFrame = document.createElement('iframe');
+    outputFrame.classList.add('codepen-frame');
+    outputFrame.setAttribute('height', '600');
+    outputFrame.setAttribute('name', 'codepen-frame');
+    outputFrame.setAttribute('style', 'border: 2px solid black; max-width: calc(100%); width: calc(100%);box-sizing: border-box;');
+    codepenable.appendChild(outputFrame);
 
-    runFrameScript();
-} else {
-    runMainScript();
-}
+    let openButtonStyles = window.getComputedStyle(openButton);
+    console.log(`-${openButtonStyles.height}`)
+    openButton.style.top = `-${openButtonStyles.height}`;
+    openButton.textContent = 'Close CodePen';
+    openButton.removeEventListener('click', openCodePen);
+    openButton.addEventListener('click', closeCodePen);
 
-function runFrameScript() {
+    let jsCode = document.querySelector('.codepen-js');
+    jsCode.setAttribute('style', 'display:none');
 
-    function undo() {
-        // hide try it
-        console.log('undo');
-        tryit.style.display = 'none';
-    
-        // show the other bits we hid
-        reset.style.display = 'block';
-        tablist.style.display = 'block';
-        outputLabel.style.display = 'block';
-        consoleContainer.style.display = 'block';
+    let js = document.querySelector('.codepen-js').textContent;
+    let css = document.querySelector('.codepen-css').textContent;
+    let html = document.querySelector('.codepen-html').textContent;
 
-        // restrict tab container width
-        tabContainer.style.width = "60%";
+    let data = {
+        title              : "document.querySelector() demo",
+        description        : "A WebAPI example from MDN",
+        html               : html,
+        css                : css,
+        js                 : js,
+        template           : true,
+        layout             : "left",
+        editors            : "001"
+      };
+  
+    let json = 
+        JSON.stringify(data)
+        // Quotes will screw up the JSON
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&apos;");
+       
+    let form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('target', 'codepen-frame');
+    form.setAttribute('action', 'https://codepen.io/pen/define');
+    form.classList.add('hidden');
 
-        // undo overflow
-        document.body.style.overflow = 'visible';
+    let input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', 'data');
+    input.setAttribute('value', json);
 
-        // tell main script to undo iframe size
-        browser.runtime.sendMessage("undo-iframe-sizing");
-      }
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
+ }
 
-    // create and add "Try it!"
-    let tryit = document.createElement('button');
-    tryit.classList.add('tryit');
-    tryit.textContent = 'Try it!';
-    tryit.style.float = 'none';
-    tryit.style.marginLeft = '200px';
-    tryit.addEventListener('click', undo)
-    let header = document.querySelector('.output-header');
-    header.appendChild(tryit);
+ function closeCodePen() {
+    openButton.style.top = '0';
+    openButton.textContent = 'Open CodePen';
+    openButton.removeEventListener('click', closeCodePen);
+    openButton.addEventListener('click', openCodePen);
 
-    // hide various bits
-    let reset = document.querySelector('.reset');
-    let tablist = document.querySelector('#tablist');
-    let outputLabel = document.querySelector('.output-label');
-    let consoleContainer = document.querySelector('.console-container ');
+    let codepenFrame = document.querySelector('.codepen-frame');
+    codepenFrame.parentNode.removeChild(codepenFrame);
 
-    reset.style.display = 'none';
-    tablist.style.display = 'none';
-    outputLabel.style.display = 'none';
-    consoleContainer.style.display = 'none';
-
-    // editor needs 100% width
-    let tabContainer = document.querySelector('#tab-container');
-    tabContainer.style.width = "100%";
-
-    // hide overflow
-    document.body.style.overflow = 'hidden';
-}
-
-function runMainScript() {
-    // resize iframe
-    let frame = document.querySelector('.interactive');
-    frame.style.width = "671px";
-    frame.style.height = "380px";
-}
-
-browser.runtime.onMessage.addListener(undoFrameSize);
-
-function undoFrameSize() {
-    if (href === main) {
-        // undo iframe sizing
-        let frame = document.querySelector('.interactive');
-        frame.style.width = "1000px";
-        frame.style.height = "692px"; 
-    }
-}
+    let jsCode = document.querySelector('.codepen-js');
+    jsCode.setAttribute('style', 'display:block');
+ }
